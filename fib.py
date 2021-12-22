48d814daa4022a91e2fa9c72e2fcdbc1c057fdd6
student <student@onlinedegree.iitm.ac.in>
2021-12-22 21:56:26 +0530

    Changed to iterative version

--- fib.py
+++ fib.py
@@ -1,12 +1,13 @@
-# Fibonacci
+# Fibonacci iterative
 def fib(n):
     """
     Returns the fibonacci value of n starting from 0
       fib(1) -> 0
     """
-    # base case
-    if n == 1:
-        return 0
-
-    # recursive case
-    return n + fib(n-1) 
+    if n == 1: return 0
+    if n == 2: return 1
+    i, j = 0, 1
+    for k in range(3, n + 1):
+        i = j
+        j = j + k
+    return j 
